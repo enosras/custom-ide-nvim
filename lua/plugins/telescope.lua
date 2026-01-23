c11a96cc9a1c11288ec6637cda7ceeb054bb6801
enos <enyamor@gmail.com>
2026-01-23 14:51:50 -0800

    made changes to match stable version

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,58 +1,59 @@
-return {
-	--  {
-	--vim.ui.select({ "apple", "banana", "cherry" }, { prompt = "Pick fruit:" }, function(choice)
-	--print("choice:", choice)
-	--end)
-	--  },
-	-- for my own gratification, delete if it fails
-
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "v0.2.0",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		-- The config function ensures the keymaps are set after the plugin loads
-		config = function()
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>es", builtin.find_files, { desc = "Telescope find files" })
-			vim.keymap.set("n", "<leader>ef", builtin.live_grep, { desc = "Telescope live grep" })
-		end,
-	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		dependencies = { "nvim-telescope/telescope.nvim" },
-		config = function()
-			require("telescope").setup({
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({
-							previewer = true,
-							winblend = 10,
-						}),
-					},
-					-- pseudo code / specification for writing custom displays, like the one
-					-- for "codeactions"
-
-					-- specific_opts = {
-					--    [kind] = {
-					--    make_indexed = function(items) -> indexed_items, width,
-					--  make_displayer = function(widths) -> displayer
-					--   make_display = function(displayer) -> function(e)
-					--    make_ordinal = function(e) -> string
-					-- },
-					-- for example to disable the custom builtin "codeactions" display
-					--   do the following
-					-- codeactions = true,
-					-- }
-					-- }
-				},
-			})
-			-- To get ui-select loaded and working with telescope, you need to call
-			-- load_extension, somewhere after setup function:
-			require("telescope").load_extension("ui-select")
-		end,
-	},
-}
-
+return {}
+--
+-- 	--  {
+-- 	--vim.ui.select({ "apple", "banana", "cherry" }, { prompt = "Pick fruit:" }, function(choice)
+-- 	--print("choice:", choice)
+-- 	--end)
+-- 	--  },
+-- 	-- for my own gratification, delete if it fails
+--
+-- 	{
+-- 		"nvim-telescope/telescope.nvim",
+-- 		tag = "v0.2.0",
+-- 		dependencies = { "nvim-lua/plenary.nvim" },
+-- 		-- The config function ensures the keymaps are set after the plugin loads
+-- 		config = function()
+-- 			local builtin = require("telescope.builtin")
+-- 			vim.keymap.set("n", "<leader>es", builtin.find_files, { desc = "Telescope find files" })
+-- 			vim.keymap.set("n", "<leader>ef", builtin.live_grep, { desc = "Telescope live grep" })
+-- 		end,
+-- 	},
+-- 	{
+-- 		"nvim-telescope/telescope-ui-select.nvim",
+-- 		dependencies = { "nvim-telescope/telescope.nvim" },
+-- 		config = function()
+-- 			require("telescope").setup({
+-- 				extensions = {
+-- 					["ui-select"] = {
+-- 						require("telescope.themes").get_dropdown({
+-- 							previewer = true,
+-- 							winblend = 10,
+-- 						}),
+-- 					},
+-- 					-- pseudo code / specification for writing custom displays, like the one
+-- 					-- for "codeactions"
+--
+-- 					-- specific_opts = {
+-- 					--    [kind] = {
+-- 					--    make_indexed = function(items) -> indexed_items, width,
+-- 					--  make_displayer = function(widths) -> displayer
+-- 					--   make_display = function(displayer) -> function(e)
+-- 					--    make_ordinal = function(e) -> string
+-- 					-- },
+-- 					-- for example to disable the custom builtin "codeactions" display
+-- 					--   do the following
+-- 					-- codeactions = true,
+-- 					-- }
+-- 					-- }
+-- 				},
+-- 			})
+-- 			-- To get ui-select loaded and working with telescope, you need to call
+-- 			-- load_extension, somewhere after setup function:
+-- 			require("telescope").load_extension("ui-select")
+-- 		end,
+-- 	},
+-- }
+--
 -- defining custom keymaps
 --local builtin = require('telescope.builtin')
 --vim.keymap.set('n', '<C-p>', builtin.find_files, {} )
