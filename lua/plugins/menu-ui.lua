32bd33d335f354d48e259cea37b51c9888f63771
enos <enyamor@gmail.com>
2026-01-24 23:51:29 -0800

    all stable updates

--- lua/plugins/menu-ui.lua
+++ lua/plugins/menu-ui.lua
@@ -3,11 +3,24 @@ return {
 	{
 		"nvzone/menu",
 		lazy = true,
-		dependencies = "nvzone/volt",
-		opts = { border = true },
+		dependencies = { "nvzone/volt", "nvim-neo-tree/neo-tree.nvim" },
+		-- opts = { border = true },
 		vim.keymap.set("n", "<leader>mn", function()
-			require("menu").open("default")
+			require("menu").open("default", { border = true })
 		end, {}),
+
+		vim.keymap.set("n", "<leader>mm", function()
+			require("menu").open("neo-tree")
+		end, {}),
+
+		-- vim.api.nvim_create_autocmd("FileType", {
+		-- 	pattern = "neo-tree",
+		-- 	callback = function()
+		-- 		vim.keymap.set("n", "<RightMouse>", function()
+		-- 			-- open menu logic (buffer-local mapping)
+		-- 		end, { buffer = true })
+		-- 	end,
+		-- }),
 		-- vim.keymap.set({ "n", "v" }, "<RightMouse>", function()
 		-- 	require("menu.utils").delete_old_menus()
 		--
@@ -17,7 +30,7 @@ return {
 		-- 	local buf = vim.api.nvim_win_get_buf(vim.fn.getmousepos().winid)
 		-- 	local options = vim.bo[buf].ft == "NvimTree" and "nvimtree" or "default"
 		--
-		-- 	require("menu").open(options, { mouse = true })
+		-- 	require("menu").open(options, { mouse = true , })
 		-- end, {}),
 	},
 }
