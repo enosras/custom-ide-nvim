11b0d93caedbcd24f917119985560f8db542c805
enos <enyamor@gmail.com>
2026-05-31 15:14:06 -0700

    some tweaks to set stage for upstream push

---
+++ lua/statuscolumn.lua
@@ -0,0 +1,186 @@
+return {}
+-- local statuscolumn = {}
+--
+-- statuscolumn.setHl = function()
+-- 	local colors =
+-- 		{ "#caa6f7", "#c1a6f1", "#b9a5ea", "#b1a4e4", "#aba3dc", "#a5a2d4", "#9fa0cc", "#9b9ec4", "#979cbc", "#949ab3" }
+--
+-- 	for i, color in ipairs(colors) do
+-- 		vim.api.nvim_set_hl(0, "Gradient_" .. i, { fg = color })
+-- 	end
+-- end
+--
+-- statuscolumn.border = function()
+-- 	-- return ">"
+-- 	-- return "%#StatusBorder#|"
+-- 	-- NOTE: lua tables start at 1 but relnum starts at 0, so we add 1 to it to get the highlight group
+-- 	if vim.v.relnum < 9 then
+-- 		return "%#Gradient_" .. (vim.v.lnum + 1) .. "#>"
+-- 	else
+-- 		return "%#Gradient_10#>"
+-- 	end
+-- end
+--
+-- statuscolumn.myStatuscolumn = function()
+-- 	local text = ""
+--
+-- 	-- Set all the custom highlight groups
+-- 	statuscolumn.setHl()
+-- 	-- This is just a different way of doing
+-- 	--
+-- 	-- text = text .. statuscolumn.border
+-- 	--
+-- 	-- This will make a lot more sense as we add more things
+-- 	-- vim.api.nvim_set_hl(0, "StatusBorder", {
+-- 	-- 	fg = "#CBA6F7",
+-- 	--	})
+-- 	text = table.concat({
+-- 		statuscolumn.border(),
+-- 		statuscolumn.number({ mode = "hybrid" }),
+-- 		--	statuscolumn.folds(),
+-- 	})
+
+-- 	return text
+-- end
+--
+-- -- statuscolumn.number = function()
+-- -- 	return vim.v.lnum
+-- -- end
+-- -- ----------- ----------------------- --------------
+-- -- ----- -- - ----------------------- ---- -----
+--
+-- -- statuscolumn.number = function(config)
+-- -- 	if config.type == "normal" then
+-- -- 		return vim.v.lnum
+-- -- 	elseif config.type == "relative" then
+-- -- 		return vim.v.relnum
+-- -- 	else
+-- -- 		-- If the relative number for a line is 0 then we know the cursor is on that line. So, we will show it's line number instead of the relative line number
+-- -- 		return vim.v.relnum == 0 and vim.v.lnum or vim.v.relnum
+-- -- 	end
+-- -- end
+--
+-- -- -------------------- --------------------------------- -----
+-- -- -----------------------------------------------------
+--
+-- statuscolumn.number = function(user_config)
+-- 	-- As a failsafe we will return an empty string if something breaks
+-- 	local text = ""
+--
+-- 	-- This is how plugins set the default options for a configuration table(an empty table is used if the user config is nil)
+-- 	-- This merges the default values and the user provided values so that you don't need to have all the keys in your config table
+-- 	local config = vim.tbl_extend("keep", user_config or {}, {
+-- 		colors = nil,
+-- 		mode = "normal",
+-- 	})
+--
+-- 	-- islist() was previously called tbl_islist() so use that if you are using an older version
+-- 	if config.colors ~= nil and vim.islist(config.colors) == true then
+-- 		for rel_numb, hl in ipairs(config.colors) do
+-- 			-- Only 1 highlight group
+-- 			if (vim.v.relnum + 1) == rel_num then
+-- 				text = "%#" .. colors .. "#"
+-- 				break
+-- 			end
+-- 		end
+--
+-- 		-- If the string is still empty then use the last color
+-- 		if text == "" then
+-- 			text = "%#" .. config.colors[#config.colors] .. "#"
+-- 		end
+-- 	end
+--
+-- 	if config.mode == "normal" then
+-- 		text = text .. vim.v.lnum
+-- 	elseif config.mode == "relative" then
+-- 		text = text .. vim.v.relnum
+-- 	elseif config.mode == "hybrid" then
+-- 		return vim.v.relnum == 0 and text .. vim.v.lnum or text .. vim.v.relnum
+-- 	end
+--
+-- 	return text
+-- end
+--
+-- --------------------------------------------------------
+-- ---------------------------------------------------
+-- statuscolumn.number = function (user_config)
+--   -- As a failsafe we will return an empty string if something breaks
+--   local text = "";
+--
+--   -- This is how plugins set the default options for a configuration table(an empty table is used if the user config is nil)
+--   -- This merges the default values and the user provided values so that you don't need to have all the keys in your config table
+--   local config = vim.tbl_extend("keep", user_config or {}, {
+--     colors = nil,
+--     mode = "normal"
+--   })
+--
+--   -- islist() was previously called tbl_islist() so use that if you are using an older version
+--   if config.colors ~= nil and vim.islist(config.colors) == true then
+--     for rel_numb, hl ipairs(config.colors) do
+--       -- Only 1 highlight group
+--       if (vim.v.relnum + 1) == rel_num then
+--         text = "%#" .. colors .. "#";
+--         break;
+--       end
+--     end
+--
+--     -- If the string is still empty then use the last color
+--     if text == "" then
+--       text = "%#" .. config.colors[#config.colors] .. "#";
+--     end
+--   end
+--
+--   if config.mode == "normal" then
+--     text = text .. vim.v.lnum;
+--   elseif config.mode == "relative" then
+--     text = text .. vim.v.relnum;
+--   elseif config.mode == "hybrid" then
+--     return vim.v.relnum == 0 and text .. vim.v.lnum or text .. vim.v.relnum;
+--   end
+--
+--   return text;
+-- end
+
+-- vim.api.nvim_set_hl(0, "MyHighlight", {
+-- 	-- Check the `nvim_set_hl()` help file to see all the available options
+-- 	fg = "#FFFFFF",
+-- 	bg = "#1E1E2E",
+-- })
+
+-- With this line we will be able to use myStatuscolumn by requiring this file and calling the function
+--
+-- statuscolumn.folds = function()
+-- 	local foldlevel = vim.fn.foldlevel(vim.v.lnum)
+-- 	local foldlevel_before = vim.fn.foldlevel((vim.v.lnum - 1) >= 1 and vim.v.lnum - 1 or 1)
+-- 	local foldlevel_after =
+-- 		vim.fn.foldlevel((vim.v.lnum + 1) <= vim.fn.line("$") and (vim.v.lnum + 1) or vim.fn.line("$"))
+--
+-- 	local foldclosed = vim.fn.foldclosed(vim.v.lnum)
+--
+-- 	-- Line has nothing to do with folds so we will skip it
+-- 	if foldlevel == 0 then
+-- 		return " "
+-- 	end
+--
+-- 	-- Line is a closed fold(I know second condition feels unnecessary but I will still add it)
+-- 	if foldclosed ~= -1 and foldclosed == vim.v.lnum then
+-- 		return "▶"
+-- 	end
+--
+-- 	-- I didn't use ~= because it couldn't make a nested fold have a lower level than it's parent fold and it's not something I would use
+-- 	if foldlevel > foldlevel_before then
+-- 		return "▽"
+-- 	end
+--
+-- 	-- The line is the last line in the fold
+-- 	if foldlevel > foldlevel_after then
+-- 		return "╰"
+-- 	end
+--
+-- 	-- Line is in the middle of an open fold
+-- 	return "╎"
+-- end
+--
+-- -- use this command to run the config `setlocal statuscolumn=%!v:lua.require('statuscolumn').myStatuscolumn()`
+--
+-- return statuscolumn
