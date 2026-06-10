f5a0ed6f541c21b4d0afcddf343e81b71c7ed2eb
enos <enyamor@gmail.com>
2026-06-10 11:02:00 -0700

    more touches for visuals

--- lua/plugins/luatab.lua
+++ lua/plugins/luatab.lua
@@ -1,69 +1,91 @@
 return {
+	-- I use this for a couple of diplay features
+	-- here is more
+	-- -----------
+	-- { "stevearc/oil.nvim" },
+	-- ---- -----
+	{
+		"mvllow/modes.nvim",
+		tag = "v0.2.1",
+		config = function()
+			require("modes").setup()
+		end,
+	},
+	{
+		"mcauley-penney/visual-whitespace.nvim",
+		event = "ModeChanged *:[vV\22]", -- optionally, lazy load on entering visual mode
+		opts = {
+			-- your opts here ...
+		},
+	},
+	{ "akinsho/bufferline.nvim", version = "*", dependencies = "nvim-tree/nvim-web-devicons" },
 	{
 		"alvarosevilla95/luatab.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 	},
+	-- { "danilamihailov/beacon.nvim" },
+	-- { "adelarsq/neoline.vim" },
 	-- ------ ------------- ---
 	-- these plugins are not related --
 	-- ----------
-	-- {
-	-- 	"nvim-lualine/lualine.nvim",
-	-- 	-- lazy = "false",
-	-- 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	-- 	--require("lualine").setup({
-	-- 	opts = {
-	-- 		options = {
-	-- 			icons_enabled = true,
-	-- 			theme = "auto",
-	-- 			component_separators = { left = "", right = "" },
-	-- 			section_separators = { left = "", right = "" },
-	-- 			disabled_filetypes = {
-	-- 				statusline = {},
-	-- 				winbar = {},
-	-- 			},
-	-- 			ignore_focus = {},
-	-- 			always_divide_middle = true,
-	-- 			always_show_tabline = true,
-	-- 			globalstatus = false,
-	-- 			refresh = {
-	-- 				statusline = 1000,
-	-- 				tabline = 1000,
-	-- 				winbar = 1000,
-	-- 				refresh_time = 16, -- ~60fps
-	-- 				events = {
-	-- 					"WinEnter",
-	-- 					"BufEnter",
-	-- 					"BufWritePost",
-	-- 					"SessionLoadPost",
-	-- 					"FileChangedShellPost",
-	-- 					"VimResized",
-	-- 					"Filetype",
-	-- 					"CursorMoved",
-	-- 					"CursorMovedI",
-	-- 					"ModeChanged",
-	-- 				},
-	-- 			},
-	-- 		},
-	-- 		sections = {
-	-- 			lualine_a = { "mode" },
-	-- 			lualine_b = { "branch", "diff", "diagnostics" },
-	-- 			lualine_c = { "filename" },
-	-- 			lualine_x = { "encoding", "fileformat", "filetype" },
-	-- 			lualine_y = { "progress" },
-	-- 			lualine_z = { "location" },
-	-- 		},
-	-- 		inactive_sections = {
-	-- 			lualine_a = {},
-	-- 			lualine_b = {},
-	-- 			lualine_c = { "filename" },
-	-- 			lualine_x = { "location" },
-	-- 			lualine_y = {},
-	-- 			lualine_z = {},
-	-- 		},
-	-- 		tabline = {},
-	-- 		winbar = {},
-	-- 		inactive_winbar = {},
-	-- 		extensions = {},
-	-- 	},
-	-- },
+	{
+		"nvim-lualine/lualine.nvim",
+		-- lazy = "false",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		-- require("lualine").setup({
+		opts = {
+			options = {
+				icons_enabled = true,
+				theme = "auto",
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
+				disabled_filetypes = {
+					statusline = {},
+					winbar = {},
+				},
+				ignore_focus = {},
+				always_divide_middle = true,
+				always_show_tabline = true,
+				globalstatus = false,
+				refresh = {
+					statusline = 1000,
+					tabline = 1000,
+					winbar = 1000,
+					refresh_time = 16, -- ~60fps
+					events = {
+						"WinEnter",
+						"BufEnter",
+						"BufWritePost",
+						"SessionLoadPost",
+						"FileChangedShellPost",
+						"VimResized",
+						"Filetype",
+						"CursorMoved",
+						"CursorMovedI",
+						"ModeChanged",
+					},
+				},
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = { "filename" },
+				lualine_x = { "encoding", "fileformat", "filetype" },
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = { "filename" },
+				lualine_x = { "location" },
+				lualine_y = {},
+				lualine_z = {},
+			},
+			tabline = {},
+			winbar = {},
+			inactive_winbar = {},
+			extensions = {},
+		},
+	},
 }
---
+++ lua/plugins/nvim-http.lua
@@ -0,0 +1,29 @@
+return {
+
+	-- {
+	-- 	"oysandvik94/curl.nvim",
+	-- 	cmd = { "CurlOpen" },
+	-- 	dependencies = {
+	-- 		"nvim-lua/plenary.nvim",
+	-- 	},
+	-- 	config = true,
+	-- },
+	-- not for http but for highlighting surround
+	-- --- --------------------
+	{
+		"kylechui/nvim-surround",
+		version = "^4.0.0", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		-- Optional: See `:h nvim-surround.configuration` and `:h nvim-surround.setup` for details
+		-- config = function()
+		--     require("nvim-surround").setup({
+		--         -- Put your configuration here
+		--     })
+		-- end
+	},
+	-- ---- --
+	-- tpopes surround which i used to replace kyle's
+	-- {
+	-- 	"https://github.com/tpope/vim-surround",
+	-- },
+}
